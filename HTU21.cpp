24d6c18b3f6e845e05e35acafe8e5e9ba0b5c146
JonasGMorsch <jonasgmorsch@gmail.com>
2019-01-24 03:18:06 -0200

    Adding Example
    Reducing unused parts derived from Adafruit and SparkFun examples from Arduino Uno

--- HTU21.cpp
+++ HTU21.cpp
@@ -1,19 +1,3 @@
-/***************************************************
-  This is a library for the HTU21DF Humidity & Temp Sensor
-
-  Designed specifically to work with the HTU21DF sensor from Adafruit
-  ----> https://www.adafruit.com/products/1899
-
-  These displays use I2C to communicate, 2 pins are required to
-  interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
- 
 #include <Wire.h>
 #include "HTU21.h"
 
---
+++ examples/HTU21_test/HTU21_test.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <HTU21.h>
+
+#define SCL D5
+#define SDA D6
+
+// Connect Vin to 3-5VDC
+// Connect GND to ground
+// Connect SCL to I2C clock pin
+// Connect SDA to I2C data pin
+
+HTU21 sensor;
+
+void setup()
+{
+  Serial.begin(250000);
+  Serial.println("HTU21 test");
+
+  if (!sensor.begin(SDA, SCL))
+  {
+    Serial.println("Couldn't find sensor!");
+    while (1);
+  }
+}
+
+void loop()
+{
+  float temp = sensor.readTemperature();
+  float rel_humidity = sensor.readHumidity();
+  
+  Serial.print("Temp: "); 
+  Serial.print(temp); 
+  Serial.print(" C");
+  
+  Serial.print("\t\t");
+  
+  Serial.print("Humidity: "); 
+  Serial.print(rel_humidity); 
+  Serial.println(" \%");
+  
+  delay(500);
+}
